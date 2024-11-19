be9517b55b01815f3d277e1a35f9a84cd5c804f9
JSellinger <132168510+JSellinger@users.noreply.github.com>
2024-11-19 11:29:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,55 @@
+from shiny import reactive, render
+from shiny.express import ui
+import random
+from datetime import datetime
+from faicons import icon_svg
+from shiny import reactive
 
+#Update interval
+UPDATE_INTERVAL_SECS: int = 1
+
+#Reactive function for updates
+
+@reactive.calc()
+def reactive_calc_combined():
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
+    # Data generation logic. Get random between -18 and -16 C, rounded to 1 decimal place
+    temp = round(random.uniform(-18, -16), 1)
+    # Get a timestamp for "now" and use string format strftime() method to format it
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
+    # Return everything we need
+    return latest_dictionary_entry
+
+#Setting up the main page layout
+ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
+
+#Setting up the sidebar layout
+with ui.sidebar(open="open"):
+    ui.h2("Antarctic Explorer", class_="text-center")
+    ui.p("A demonstration of real-time temperature readings in Antarctica.", class_="text-center",)
+
+ui.h2("Current Temperature")
+
+#Rendering main body text - display temp
+@render.text
+def display_temp():
+    """Get the latest reading and return a temperature string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['temp']} C"
+
+ui.p("warmer than usual")
+
+icon_svg("sun")
+
+ui.hr()
+
+ui.h2("Current Date and Time")
+
+#Rendering main body text - display time
+@render.text
+def display_time():
+    """Get the latest reading and return a timestamp string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['timestamp']}"
